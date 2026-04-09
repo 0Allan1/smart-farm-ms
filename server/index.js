5766c4ef855bc60983113367e8ed8c330893b302
0Allan1 <a.tumusime@alustudent.com>
2026-04-09 20:58:41 +0200

    updates

--- server/index.js
+++ server/index.js
@@ -19,7 +19,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGIN || 'http://localhost:5173'
+}));
 app.use(express.json());
 
 // Routes
